bfc6e64bcc1c632cb3444fef70a39fc8c07a1749
muharrem-kacan <muharrem@gameon.app>
2022-05-31 11:28:40 +0300

    app changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this is deployed with github actions
         </a>
       </header>
     </div>
