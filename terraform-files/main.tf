e797399c49c58796d08c31100f6d2f4937ec343e
muharrem-kacan <muharrem@gameon.app>
2022-05-31 17:02:42 +0300

    new one

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -6,9 +6,8 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = "ami-045b15cc228b53edd"
   instance_type = var.instance_type
-  vpc_security_group_ids = ["sg-0b5cba222cef5b125"]
   tags = {
     Name = var.instance_name
   }
-  
+
 }
