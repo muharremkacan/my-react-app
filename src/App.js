6e2a1be4f6c394e58f86e66363aba2f975331aa0
muharrem-kacan <muharrem@gameon.app>
2022-05-31 13:50:11 +0300

    push and deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          modified one
+          push and deploy to ec2
         </a>
       </header>
     </div>
