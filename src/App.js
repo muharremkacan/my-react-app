29dd61485e3ac5646ba20a847c9995032e2bc9ed
muharrem-kacan <muharrem@gameon.app>
2022-05-31 13:40:47 +0300

    new

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          this is the test one
+          modified one
         </a>
       </header>
     </div>
