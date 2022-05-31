e9d0db96b79ce16d5584a8cb1724f9eee906fe62
muharrem-kacan <muharrem@gameon.app>
2022-05-31 16:39:55 +0300

    terraform

--- terraform-files/versions.tf
+++ terraform-files/versions.tf
@@ -1,13 +1,11 @@
 terraform {
   required_version = ">= 0.12"
-
-/*
+  
   backend "s3" {
     bucket = "timothybucket2123"
     key = "xxx.tfstate"
     region = "us-east-1"
  #   dynamodb_table = "timothytest"
-  }
-*/
+}
 }
 
